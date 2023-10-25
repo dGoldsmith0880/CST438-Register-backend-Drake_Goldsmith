f52096bd857aeb7399842b0356a89fb97d8199b6
Drake Goldsmith <123588852+dGoldsmith0880@users.noreply.github.com>
2023-10-24 21:03:44 -0700

    Update StudentDTO.java

--- cst438_registration/src/main/java/com/cst438/domain/StudentDTO.java
+++ cst438_registration/src/main/java/com/cst438/domain/StudentDTO.java
@@ -1,10 +1,4 @@
 package com.cst438.domain;
 
-public record StudentDTO(int studentId, String name, String email, int statusCode, String status){
+public record StudentDTO(int studentId, String name, String email, int statusCode, String status, String password, String role) {
 }
-
-//package com.cst438.domain;
-//
-//public record StudentDTO (int student_id, String name, String email, int status_code, String status){
-//
-//}
