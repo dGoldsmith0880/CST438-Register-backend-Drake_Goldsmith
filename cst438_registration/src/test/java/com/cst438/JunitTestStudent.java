3cad5449405f0353136cd934a209ef2b56b7097a
Drake Goldsmith <123588852+dGoldsmith0880@users.noreply.github.com>
2023-10-24 21:07:14 -0700

    Update JunitTestStudent.java

--- cst438_registration/src/test/java/com/cst438/JunitTestStudent.java
+++ cst438_registration/src/test/java/com/cst438/JunitTestStudent.java
@@ -26,7 +26,7 @@ public class JunitTestStudent {
 	
 	@Test
 	public void createStudent() throws Exception {
-		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null);
+		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null, null, null);
 		MockHttpServletResponse response;
 
 		response = mvc.perform(
@@ -63,7 +63,7 @@ public class JunitTestStudent {
 	
 	@Test
 	public void createStudentDupEmail() throws Exception {
-		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null);
+		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null, null, null);
 		MockHttpServletResponse response;
 
 		response = mvc.perform(
@@ -78,7 +78,7 @@ public class JunitTestStudent {
 		assertTrue(student_id > 0);
 		
 		// try to create another student with same email
-		sdto = new StudentDTO(0, "name2 test2", "ntest@csumb.edu", 0, null);
+		sdto = new StudentDTO(0, "name2 test2", "ntest@csumb.edu", 0, null, null, null);
 		response = mvc.perform(
 				MockMvcRequestBuilders
 			      .post("/student")
@@ -113,7 +113,7 @@ public class JunitTestStudent {
 		assertEquals(200, response.getStatus());
 		StudentDTO original = fromJsonString(response.getContentAsString(), StudentDTO.class);
 		// modify name, email and statusCode
-		StudentDTO mod = new StudentDTO(original.studentId(), "new name", "newname@csumb.edu", 1, "balance outstanding");
+		StudentDTO mod = new StudentDTO(original.studentId(), "new name", "newname@csumb.edu", 1, "balance outstanding", null, null);
 		response = mvc.perform(
 				MockMvcRequestBuilders
 			      .put("/student/2")
@@ -138,7 +138,7 @@ public class JunitTestStudent {
 	public void updateStudentDupEmail() throws Exception {
 		// create 2 students 
 		
-		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null);
+		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null, null, null);
 		MockHttpServletResponse response;
 
 		response = mvc.perform(
@@ -152,7 +152,7 @@ public class JunitTestStudent {
 		int  student_id = Integer.parseInt(response.getContentAsString());
 		assertTrue(student_id > 0);
 		
-		StudentDTO sdto2 = new StudentDTO(0, "name test2", "ntest2@csumb.edu", 0, null);
+		StudentDTO sdto2 = new StudentDTO(0, "name test2", "ntest2@csumb.edu", 0, null, null, null);
 		response = mvc.perform(
 				MockMvcRequestBuilders
 			      .post("/student")
@@ -165,7 +165,7 @@ public class JunitTestStudent {
 		assertTrue(student_id2 > 0);
 		
 		// attempt to change email of student #1 to student #2
-		StudentDTO sdto3 = new StudentDTO(student_id, "name test", "ntest2@csumb.edu", 0, null);
+		StudentDTO sdto3 = new StudentDTO(student_id, "name test", "ntest2@csumb.edu", 0, null, null, null);
 		response = mvc.perform(
 				MockMvcRequestBuilders
 			      .put("/student/"+student_id)
@@ -180,7 +180,7 @@ public class JunitTestStudent {
 	
 	@Test
 	public void updateStudentNotFound() throws Exception {
-		StudentDTO sdto = new StudentDTO(99, "namenew test", "ntestnew@csumb.edu", 0, null);
+		StudentDTO sdto = new StudentDTO(99, "namenew test", "ntestnew@csumb.edu", 0, null, null, null);
 		MockHttpServletResponse response;
 
 		response = mvc.perform(
@@ -196,7 +196,7 @@ public class JunitTestStudent {
 	
 	@Test
 	public void deleteStudentNoEnrollments() throws Exception {
-		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null);
+		StudentDTO sdto = new StudentDTO(0, "name test", "ntest@csumb.edu", 0, null, null, null);
 		MockHttpServletResponse response;
 
 		response = mvc.perform(
@@ -262,148 +262,3 @@ public class JunitTestStudent {
 		}
 	}
 }
-//package com.cst438;
-//import com.cst438.domain.Student;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.mock.web.MockHttpServletResponse;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import static org.junit.Assert.assertEquals;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class JunitTestStudent {
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    // Test to retrieve all students "getAllStudents"
-//    @Test
-//    public void getAllStudents() throws Exception {
-//        // Perform a GET request to retrieve all students
-//        MockHttpServletResponse response = mvc.perform(
-//                MockMvcRequestBuilders
-//                        .get("/students/")
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andReturn().getResponse();
-//        
-//        // Verify that return status is OK (value 200)
-//        assertEquals(200, response.getStatus());
-//        Student[] studentList = fromJsonString(response.getContentAsString(), Student[].class);
-//        assertEquals(4, studentList.length);
-//        
-//        for (Student student : studentList) {
-//            System.out.println(student.toString());
-//        }
-//    }
-//    
-//    @Test
-//    public void updateStudentStatus() throws Exception {
-//        Student student = new Student();
-//        student.setName("Drake Goldsmith");
-//        student.setEmail("dGoldsmith@csumb.edu");
-//        student.setStatusCode(1);
-//        student.setStatus("Active");
-//
-//        // Perform a POST request to create a student
-//        MockHttpServletResponse response = mvc.perform(
-//                MockMvcRequestBuilders
-//                        .post("/students/")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(asJsonString(student))
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andReturn().getResponse();
-//
-//        // Verify that return status is OK (value 200)
-//        assertEquals(200, response.getStatus());
-//
-//        Student createdStudent = fromJsonString(response.getContentAsString(), Student.class);
-//        int createdStudentId = createdStudent.getStudent_id();
-//
-//        // Perform a PUT request to update the student's status
-//        Student updatedStudent = new Student();
-//        updatedStudent.setStatusCode(0); // Updated status code
-//        updatedStudent.setStatus("Inactive"); // Updated status
-//        response = mvc.perform(
-//                MockMvcRequestBuilders
-//                        .put("/students/" + createdStudentId)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(asJsonString(updatedStudent))
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andReturn().getResponse();
-//
-//        // Verify that return status is OK (value 200) when updating the student's status
-//        assertEquals(200, response.getStatus());
-//        
-//        // Perform a GET request to retrieve student data based off of ID
-//        response = mvc.perform(
-//                MockMvcRequestBuilders
-//                        .get("/students/" + createdStudentId)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andReturn().getResponse();
-//
-//        // Verify that return status is OK (value 200) when retrieving the student by ID
-//        assertEquals(200, response.getStatus());
-//
-//        Student retrievedStudent = fromJsonString(response.getContentAsString(), Student.class);
-//        assertEquals(createdStudentId, retrievedStudent.getStudent_id());
-//        assertEquals(0, retrievedStudent.getStatusCode()); // Updated status code
-//        assertEquals("Inactive", retrievedStudent.getStatus()); // Updated status
-//    }
-//
-//    @Test
-//    public void deleteStudent() throws Exception {
-//        Student student = new Student();
-//        student.setName("Drake Goldsmith");
-//        student.setEmail("dGoldsmith@csumb.edu");
-//        student.setStatusCode(1);
-//        student.setStatus("Active");
-//
-//        // Perform a POST request to create a student
-//        MockHttpServletResponse response = mvc.perform(
-//                MockMvcRequestBuilders
-//                        .post("/students/")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(asJsonString(student))
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andReturn().getResponse();
-//
-//        // Verify that return status is OK (value 200)
-//        assertEquals(200, response.getStatus());
-//
-//        // Retrieve the created student
-//        Student createdStudent = fromJsonString(response.getContentAsString(), Student.class);
-//        int createdStudentId = createdStudent.getStudent_id();
-//
-//        // Perform a DELETE request to delete the student by ID
-//        response = mvc.perform(
-//                MockMvcRequestBuilders
-//                        .delete("/students/" + createdStudentId))
-//                .andReturn().getResponse();
-//
-//        // Verify that return status is OK (value 200) when deleting the student by ID
-//        assertEquals(200, response.getStatus());
-//    }
-//    
-//
-//    private static String asJsonString(final Object obj) {
-//        try {
-//            return new ObjectMapper().writeValueAsString(obj);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    private static <T> T fromJsonString(String str, Class<T> valueType) {
-//        try {
-//            return new ObjectMapper().readValue(str, valueType);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
