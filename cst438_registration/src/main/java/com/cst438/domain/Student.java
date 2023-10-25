058a330b71617c713e1cf041cee016f59c7a363c
Drake Goldsmith <123588852+dGoldsmith0880@users.noreply.github.com>
2023-10-24 21:03:22 -0700

    Update Student.java

--- cst438_registration/src/main/java/com/cst438/domain/Student.java
+++ cst438_registration/src/main/java/com/cst438/domain/Student.java
@@ -7,53 +7,80 @@ import javax.persistence.Id;
 
 @Entity
 public class Student {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int student_id;
-	private String name;
-	private String email;
-	private int statusCode;
-	private String status;
-	
-	public Student() {
-		super();
-	}
-	
-	public int getStudent_id() {
-		return student_id;
-	}
-	public void setStudent_id(int student_id) {
-		this.student_id = student_id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	@Override
-	public String toString() {
-		return "Student [student_id=" + student_id + ", name=" + name + ", email=" + email + ", statusCode="
-				+ statusCode + ", status=" + status + "]";
-	}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int student_id;
+    private String name;
+    private String email;
+    private int statusCode;
+    private String status;
+    private String password;
+    private String role;
+
+    public Student() {
+        super();
+    }
+
+    public int getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [student_id=" + student_id + ", name=" + name + ", email=" + email + ", statusCode="
+                + statusCode + ", status=" + status + ", password=" + password + ", role=" + role + "]";
+    }
 }
